370e64e3c499183e34038ee108ee85ba3837020c
Gilcemir <gangelofilho@gmail.com>
2022-04-04 07:49:44 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using Tree;
+using System;
+
+TreeNode<int> nodo = new TreeNode<int>();
+
+nodo.right = new TreeNode<int>(1);
+nodo.left = new TreeNode<int>(2);
+
+Console.WriteLine("----");
+Console.WriteLine(nodo.val);
+Console.WriteLine(nodo.left.val);
+Console.WriteLine(nodo.right.val);
---
+++ src/Tree.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Tree
+{
+    public class TreeNode<T>
+        where T : IComparable
+    {
+        public T val { get; set;}
+        public TreeNode<T> left { get; set;}
+        public TreeNode<T> right { get; set;}
+
+        public TreeNode(T val = default(T), TreeNode<T> left = null, TreeNode<T> right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public class BinarySearchTree<T>
+        where T : IComparable
+    {
+        public TreeNode<T> root { get; set;}
+
+        public BinarySearchTree()
+        {
+            this.root = new TreeNode<T>();
+        }
+
+    }
+}
